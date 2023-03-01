11a0ee6399d942db65c7b01220569c1b3822cc72
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2023-03-01 17:12:52 +0900

    feat: add godotenv

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+)
+
 func main() {
-	println("Hello, world!")
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	key := os.Getenv("API_KEY")
+	if key == "" {
+		log.Fatal("API_KEY is not set")
+	}
+	fmt.Println(key)
 }
