6f493a3d86bcfbae55c07eee2e1ec086950b25d8
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2023-03-09 23:20:13 +0900

    chore: delete function

--- main.go
+++ main.go
@@ -1,31 +1,16 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"errors"
 	"log"
 	"os"
-	"time"
 
-	"github.com/Sigumaa/lfu"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	key, username, err := loadConfig()
 	if err != nil {
-		log.Fatal(err)
-	}
-
-	client := lfu.New(username, key)
-	log.Println("---friends---")
-	if err := friends(client); err != nil {
-		log.Fatal(err)
-	}
-	time.Sleep(1 * time.Second)
-	log.Println("---info---")
-	if err := info(client); err != nil {
 		log.Fatal(err)
 	}
 
@@ -44,15 +29,4 @@ func loadConfig() (string, string, error) {
 		return "", "", errors.New("USER_NAME is not set")
 	}
 	return key, username, nil
-}
-
-func printJSON(v interface{}) error {
-	var buf bytes.Buffer
-	enc := json.NewEncoder(&buf)
-	enc.SetIndent("", "  ")
-	if err := enc.Encode(v); err != nil {
-		return err
-	}
-	log.Println(buf.String())
-	return nil
 }
